59ebc4ba2d9ac3ced4b379fca08b706f8bc00a65
Romain Dortier <romain.dortier@gmail.com>
2021-04-10 10:02:11 +0200

    Update codeBlock.js

--- src/components/mdxComponents/codeBlock.js
+++ src/components/mdxComponents/codeBlock.js
@@ -30,8 +30,7 @@ const CodeBlock = ({ children: exampleCode, ...props }) => {
     return <LoadableComponent code={exampleCode} />;
   } else {
     return (
-      <Highlight {...defaultProps} code={exampleCode} language="jsx">
-      </Highlight>
+      <div></div>
     );
   }
 };
